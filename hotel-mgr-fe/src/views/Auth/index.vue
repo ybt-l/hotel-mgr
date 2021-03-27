eac6fddf14160770894c18b8ddb3f988f2f56b2e
Ybt <1979171879@qq.com>
2021-03-27 14:40:32 +0800

    完成登录注册的页面

---
+++ hotel-mgr-fe/src/views/Auth/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="auth">
+    <div class="bg"></div>
+    <div class="title-info">
+      <img src="../../assets/title.jpg" alt="" />
+      <h2 class="title">酒店管理系统 后台</h2>
+    </div>
+
+    <div class="form">
+      <a-tabs>
+        <a-tab-pane key="1" tab="登录">
+          <div class="item">
+            <a-input size="large" placeholder="账户">
+              <template v-slot:prefix>
+                <UserOutlined />
+              </template>
+            </a-input>
+          </div>
+
+          <div class="item">
+            <a-input size="large" placeholder="密码">
+              <template v-slot:prefix>
+                <LockOutlined />
+              </template>
+            </a-input>
+          </div>
+
+          <div class="item">
+            <a href="">忘记密码</a>
+          </div>
+
+          <div class="item">
+            <a-button size="large" type="primary"> 登录 </a-button>
+          </div>
+        </a-tab-pane>
+        <a-tab-pane key="2" tab="注册">
+          <div class="item">
+            <a-input size="large" placeholder="账户">
+              <template v-slot:prefix>
+                <UserOutlined />
+              </template>
+            </a-input>
+          </div>
+
+          <div class="item">
+            <a-input size="large" placeholder="密码">
+              <template v-slot:prefix>
+                <LockOutlined />
+              </template>
+            </a-input>
+          </div>
+
+          <div class="item">
+            <a-input size="large" placeholder="邀请码">
+              <template v-slot:prefix>
+                <MailOutlined />
+              </template>
+            </a-input>
+          </div>
+
+          <div class="item">
+            <a href="">忘记密码</a>
+          </div>
+
+          <div class="item">
+            <a-button size="large" type="primary"> 注册</a-button>
+          </div>
+        </a-tab-pane>
+      </a-tabs>
+    </div>
+  </div>
+</template>
+
+
+<script src='./index.js'></script>
+
+<style lang="scss" scoped>
+  @import './index.scss';
+</style>
